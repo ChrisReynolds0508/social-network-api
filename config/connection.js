c5d75bc5b820a3ecb648af1bd3f0153eb6a5620a
Chrisreynolds0508 <Chris.reynolds0508@gmail.com>
2024-10-08 22:06:58 +1100

    added all routes and controllers

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://127.0.0.1:27017/studentsDB';
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-connect(connectionString);
 
-module.exports = connection;
+module.exports = mongoose.connection;
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const db = require('./config/connection');  
-
-// require models below
-
+// Import required packages and files
+const express =  require('express');
+const db = require('./config/connection');
+const routes = require('./routes/index');
+// Set up environment variables
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-app.use(express.json());
+// Use middleware to parse incoming data
 app.use(express.urlencoded({ extended: true }));
-
-// require routes below
-
+app.use(express.json());
+// Use routes defined in routes.js
+app.use(routes); 
+// Connect to the MongoDB database and start the server
 db.once('open', () => {
     app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
+      console.log(`API server running on port ${PORT}!`);
     });
-    });
+  });
